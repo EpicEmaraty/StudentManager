4c14e75ccb38164d472ed846589f5d85cc285c07
epicemaraty <realnuaimi@mail.csuchico.edu>
2018-04-30 19:35:51 -0700

    student.h

---
+++ student.h
@@ -0,0 +1,23 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+#include <iostream>
+#include <string> 
+
+using namespace std; 
+
+class student
+{
+
+private:
+
+string firstName;
+string lastName;
+
+
+public:
+void person.setName(string, string);
+void person.fullName();
+
+};
+
+#endif
